629971a12494a5c8980762c3349b4ccc97ba1d7f
lolesia <alesyavaskovith@gmail.com>
2023-12-25 00:20:03 +0200

    added parameterization

--- tests/login/test_login.py
+++ tests/login/test_login.py
@@ -1,7 +1,46 @@
 from conftest import user_login_fixture
 import logging
-
+import pytest
 logging.basicConfig(level=logging.INFO)
+
+
+# class TestLogin:
+#     """
+#     Class to implement testing of
+#     basic authentication functions
+#     """
+#
+#     def test_user_login_positive(self, user_login_fixture):
+#         login = 'qa.ajax.app.automation@gmail.com'
+#         password = 'qa_automation_password'
+#         login_page = user_login_fixture.login(login, password)
+#
+#         try:
+#             assert login_page.is_logged_in() is True
+#         except AssertionError as e:
+#             logging.error(f"Assertion error: {e}")
+#             raise
+#         finally:
+#             login_page.logout()
+#
+#     def test_user_login_invalid_mail_format(self, user_login_fixture):
+#         login = 'qa.ajax.app.automation'
+#         password = 'qa_automation_password'
+#         try:
+#             assert user_login_fixture.login(login, password).authentication_error() == 'Неверный формат электронной почты'
+#         except AssertionError as e:
+#             logging.error(f"Assertion error: {e}")
+#             raise
+#
+#     def test_user_login_invalid_login_data(self, user_login_fixture):
+#         login = 'invalid_login_automation@gmail.com'
+#         password = 'invalid_password'
+#         error_message = user_login_fixture.login_second_screen(login, password).authentication_error()
+#         try:
+#             assert error_message == 'Неверный логин или пароль' or error_message == 'Подождите, идет синхронизация с сервером'
+#         except AssertionError as e:
+#             logging.error(f"Assertion error: {e}")
+#             raise
 
 
 class TestLogin:
@@ -10,38 +49,23 @@ class TestLogin:
     basic authentication functions
     """
 
-    def test_user_login_positive(self, user_login_fixture):
-        login = 'qa.ajax.app.automation@gmail.com'
-        password = 'qa_automation_password'
-        login_page = user_login_fixture.login(login, password)
+    @pytest.mark.parametrize("login, password, login_method, expected_error", [
+        ('qa.ajax.app.automation@gmail.com', 'qa_automation_password', 'login', None),
+        ('qa.ajax.app.automation', 'qa_automation_password', 'login', 'Неверный формат электронной почты'),
+        ('invalid_login_automation@gmail.com', 'invalid_password', 'login_second_screen', 'Неверный логин или пароль'),
+    ])
+    def test_user_login(self, user_login_fixture, login, password, login_method, expected_error):
+        login_page = getattr(user_login_fixture, login_method)(login, password)
 
         try:
-            assert login_page.is_logged_in() is True
+            if expected_error is None:
+                assert login_page.is_logged_in() is True
+                login_page.logout()
+            else:
+                assert login_page.authentication_error() == expected_error
         except AssertionError as e:
-            logging.error(f"Assertion error: {e}")
+            logging.error(f"Ошибка проверки: {e}")
             raise
-        finally:
-            login_page.logout()
-
-    def test_user_login_invalid_mail_format(self, user_login_fixture):
-        login = 'qa.ajax.app.automation'
-        password = 'qa_automation_password'
-        try:
-            assert user_login_fixture.login(login, password).authentication_error() == 'Неверный формат электронной почты'
-        except AssertionError as e:
-            logging.error(f"Assertion error: {e}")
-            raise
-
-    def test_user_login_invalid_login_data(self, user_login_fixture):
-        login = 'invalid_login_automation@gmail.com'
-        password = 'invalid_password'
-        try:
-            assert user_login_fixture.login_second_screen(login, password).authentication_error() == 'Неверный логин или пароль'
-        except AssertionError as e:
-            logging.error(f"Assertion error: {e}")
-            raise
-
-
 
 
 
